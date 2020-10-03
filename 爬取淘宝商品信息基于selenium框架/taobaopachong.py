86eb2517c084c1b0b439b4e74824f24f9b139a95
hao <960751327@qq.com>
2020-10-03 17:41:32 +0800

    维护淘宝爬虫代码

--- 爬取淘宝商品信息基于selenium框架/taobaopachong.py
+++ 爬取淘宝商品信息基于selenium框架/taobaopachong.py
@@ -3,7 +3,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from urllib.parse import quote
+from urllib import parse
+import pandas as pd
 from pyquery import PyQuery
 import time
 import json
@@ -11,7 +12,10 @@ import re
 #定义一个变量，最好用大写这个是约定俗成
 KEYWORD = '月饼'
 #定位Chromedriver这个工具的位置
-browser = webdriver.Chrome(executable_path="C:\\Users\\96075\\Desktop\\全部资料\\Python\\爬虫\\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("prefs",{"profile.mamaged_default_content_settings.images":2})
+options.add_experimental_option('excludeSwitches',['enable-automation'])
+browser = webdriver.Chrome(executable_path="C:\\Users\\96075\\Desktop\\全部资料\\Python\\爬虫\\chromedriver.exe",options=options)
 #设置等待时间
 wait = WebDriverWait(browser,10)
 url ='https://www.taobao.com/'
@@ -19,67 +23,83 @@ url ='https://www.taobao.com/'
 def crawl_page():
     try:
         browser.get(url)#获取网页
+        #用xpath语法定位到输入框
         input = wait.until(EC.presence_of_element_located((
             By.XPATH,'//*[@id="q"]'
-        )))#等到输入框加载出来
+        )))
+        #用xpath语法定位到搜索框
         button = wait.until(EC.element_to_be_clickable((
             By.XPATH,'//*[@id="J_TSearchForm"]/div[1]/button'
-        )))#等到搜索框加载出来
+        )))
         input.send_keys(KEYWORD)#输入关键词
         button.click()#模拟鼠标点击
+        #等到python爬虫页面的总页数加载出来
         total = wait.until(EC.presence_of_element_located((
             By.XPATH,'//*[@id="mainsrp-pager"]/div/div/div/div[1]'
-        ))).text#等到python爬虫页面的总页数加载出来
-        total = re.sub(r',|，','',total)#发现总页数有逗号
+        ))).text
+        # 发现总页数有逗号
+        total = re.sub(r',|，','',total)
         #数据清洗，将共100页后面的逗号去掉,淘宝里的是大写的逗号
         print(total)
         totalnum = int(re.compile('(\d+)').search(total).group(1))
         # 只取出100这个数字
         print("第1页:")
-        get_products()#获取数据(下面才写到)
+        # 获取数据
+        get_products()
+        #返回总的页数
         return totalnum
     except:
         crawl_page()
 
 def get_products():
-    #用工具去爬取这个页面
+    list_price = []
+    list_title = []
+    list_deal = []
+    list_picture = []
+    # 用工具去爬取这个页面
     html = browser.page_source
-    #打印这个页面的信息
+    # 打印这个页面的信息
     doc = PyQuery(html)
-    #去定位获取我们需要的信息
+    # 去定位获取我们需要的信息
     items = doc("#mainsrp-itemlist .items .item").items()
     for item in items:
-        product ={
-            "image": item.find(".img").attr("data-src"),
-            "price": item.find(".price").text(),
-            "deal": item.find(".deal-cnt").text(),
-            "title": item.find(".title").text(),
-            "shop": item.find(".shop").text(),
-            "location": item.find(".location").text(),
-        }
-        print(product)
-        #保存我们获取的信息
-        save_to_file(product)
-#写一个保存文件的函数
-def save_to_file(result):
-    #a是追加信息的意思
-    with open("result.text","a",encoding='utf-8') as f:
-        #把python转化为json，然后用json的形式保存下来，ensure_ascii=False是识别有没有中文的意思
-        f.write(json.dumps(result,ensure_ascii=False)+"\n")
-        print("存储到text成功")
+        price = item.find(".price").text(),
+        list_price.append(price)
+        title = item.find(".title").text(),
+        list_title.append(title)
+        deal = item.find(".deal-cnt").text(),
+        list_deal.append(deal)
+        picture = parse.urljoin('http:', item.find(".img").attr("data-src"))
+        list_picture.append(picture)
+    df = pd.DataFrame()
+    df["商品名称"] = list_title
+    df["商品价格"] = list_price
+    df["商品销量"] = list_deal
+    df["图片链接"] = list_picture
+    try:
+        df.to_csv("商品的基本信息.csv", mode="a+", header=None, index=None, encoding="utf-8")
+        print("写入成功")
+    except:
+        print("当页数据写入失败")
 
 def next_page():
-    totalnum = crawl_page()#获取总页数的值，并且调用search获取第一页数据
-    num = 1#初始为1，因为我第一页已经获取过数据了
-    while num != totalnum - 1:#首先进来的是第1页，共100页，所以只需要翻页99次
+    # 获取总页数的值，并且调用search获取第一页数据
+    totalnum = crawl_page()
+    # 初始为1，因为我第一页已经获取过数据了
+    num = 1
+    # 首先进来的是第1页，共100页，所以只需要翻页99次
+    while num != totalnum - 1:
         print("第%s页:" %str(num+1) )
+        # 用修改s属性的方式翻页
         browser.get('https://s.taobao.com/search?q={}&s={}'.format(KEYWORD,44 * num))
-        #用修改s属性的方式翻页
+        # 等待10秒
         browser.implicitly_wait(10)
-        #等待10秒
-        get_products()#获取数据
+        # 获取数据
+        get_products()
+        #延迟3秒
         time.sleep(3)
-        num +=1#自增
+        # 自增
+        num +=1
 
 #写一个循环函数，用于爬取多页信息的内容
 def main():
--- 爬取淘宝商品信息基于selenium框架/爬取商品属性.py
+++ 爬取淘宝商品信息基于selenium框架/爬取商品属性.py
@@ -3,16 +3,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from urllib.parse import quote
 from pyquery import PyQuery
 import time
-import pandas as pd
-import requests
-from  urllib import parse
 import json
+import re
 
 #定义一个变量，最好用大写这个是约定俗成
-KEYWORD = input("请输入要搜索的关键词:")
+KEYWORD = '月饼'
 #定位Chromedriver这个工具的位置
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs",{"profile.mamaged_default_content_settings.images":2})
@@ -20,35 +17,60 @@ options.add_experimental_option('excludeSwitches',['enable-automation'])
 browser = webdriver.Chrome(executable_path="C:\\Users\\96075\\Desktop\\全部资料\\Python\\爬虫\\chromedriver.exe",options=options)
 #设置等待时间
 wait = WebDriverWait(browser,10)
+url ='https://www.taobao.com/'
 
 list_property = []
-
-def crawl_page(page):
+#先写一个去调动输入框定位一个商品类型的函数，并且获取总的页数
+def crawl_page():
     try:
-        #爬取这个页面，quote的作用就是将我们输入的文字转化成计算机看得懂的文字
-        url = "https://s.taobao.com/search?q="+quote(KEYWORD)
-        #然后用我们的工具去获取这个页面的信息
-        browser.get(url)
-        time.sleep(5)
-        #如果页面大于1，则执行下一步
-        if page>1:
-            #先是定位到下一页的位置，这里我们用到的是selenium语法
-            page_box = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"input.input.J_Input")))
-            #然后点击按钮，实现页面跳转
-            sumbit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"span.btn.J_Submit")))
-            #然后关闭
-            page_box.clear()
-            #发送页数到page这个参数里面
-            page_box.send_keys(page)
-            #停止点击事件
-            sumbit_button.click()
-        #等待这个页面的施行
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".m-itemlist .items .item")))
-        #去爬取这个页面的信息
+        browser.get(url)#获取网页
+        #用xpath语法定位到输入框
+        input = wait.until(EC.presence_of_element_located((
+            By.XPATH,'//*[@id="q"]'
+        )))
+        #用xpath语法定位到搜索框
+        button = wait.until(EC.element_to_be_clickable((
+            By.XPATH,'//*[@id="J_TSearchForm"]/div[1]/button'
+        )))
+        input.send_keys(KEYWORD)#输入关键词
+        button.click()#模拟鼠标点击
+        #等到python爬虫页面的总页数加载出来
+        total = wait.until(EC.presence_of_element_located((
+            By.XPATH,'//*[@id="mainsrp-pager"]/div/div/div/div[1]'
+        ))).text
+        # 发现总页数有逗号
+        total = re.sub(r',|，','',total)
+        #数据清洗，将共100页后面的逗号去掉,淘宝里的是大写的逗号
+        print(total)
+        totalnum = int(re.compile('(\d+)').search(total).group(1))
+        # 只取出100这个数字
+        print("第1页:")
+        # 获取数据
         get_products()
+        #返回总的页数
+        return totalnum
     except:
-        crawl_page(page)
-
+        crawl_page()
+#然后根据获取总的页数来获取每一页商品的内容和信息
+def next_page():
+    # 获取总页数的值，并且调用search获取第一页数据
+    totalnum = crawl_page()
+    # 初始为1，因为我第一页已经获取过数据了
+    num = 1
+    # 首先进来的是第1页，共100页，所以只需要翻页99次
+    while num != totalnum - 1:
+        print("第%s页:" %str(num+1) )
+        # 用修改s属性的方式翻页
+        browser.get('https://s.taobao.com/search?q={}&s={}'.format(KEYWORD,44 * num))
+        # 等待10秒
+        browser.implicitly_wait(10)
+        # 获取数据
+        get_products()
+        #延迟3秒
+        time.sleep(3)
+        # 自增
+        num +=1
+#再根据获取到的商品信息来定位来商品的链接从而进入商品的详细页面
 def get_products():
     #用工具去爬取这个页面
     html = browser.page_source
@@ -64,7 +86,7 @@ def get_products():
         url = "https:{}".format(href).replace("https:https:","https:")
         get_url(url)
 
-#http://g-search3.alicdn.com/img/bao/uploaded/i4/i1/652684261/O1CN01SNuqML1hLaFlM5ijz_!!652684261.jpg_460x460Q90.jpg_.webp
+#获取到详细页面之后去获取每个商品信息的属性，并且把这些属性抓取下来
 def get_url(url):
     browser.get(url)
     time.sleep(2)
@@ -80,17 +102,10 @@ def get_url(url):
 #写一个保存文件的函数
 def save_to_file(result):
     #a是追加信息的意思
-    with open("result.text","a",encoding='utf-8') as f:
+    with open("商品属性.text","a",encoding='utf-8') as f:
         #把python转化为json，然后用json的形式保存下来，ensure_ascii=False是识别有没有中文的意思
         f.write(json.dumps(result,ensure_ascii=False)+"\n")
         print("存储到text成功")
 
-
-#最大页数
-MAX_PAGE = 100
-#写一个循环函数，用于爬取多页信息的内容
-def main():
-    for i in range(1,MAX_PAGE+1):
-        crawl_page(i)
 if __name__ == '__main__':
-    main()
+    next_page()
